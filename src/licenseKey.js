e378af68fe588f8a0e7c096dc631a55c257c3089
Nolan Bridges <nolanmbridges@gmail.com>
2021-06-04 00:46:57 -0700

    Update licenseKey.js

--- src/licenseKey.js
+++ src/licenseKey.js
@@ -1,6 +1,8 @@
 const keyTextBox = document.getElementById('keyTextBox');
 const returnBtn = document.getElementById('returnBtn');
 
+var mysql = require('mysql');
+
 keyTextBox.addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode == 13) {
@@ -10,5 +12,16 @@ keyTextBox.addEventListener("keyup", function(event) {
 returnBtn.onclick = keySearched;
 
 function keySearched() {
-    console.log("YOOO");
+    var con = mysql.createConnection({
+        host: "3.221.75.247",
+        user: "typaCC",
+        password: "sM1L6::@"
+    });
+      
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+    });
+
+    // check if license key is valid
 }
