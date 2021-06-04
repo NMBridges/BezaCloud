64b15afff631821694deb6d734eb7fc06389c364
Nolan <nolanmbridges@gmail.com>
2021-06-03 23:43:32 -0700

    v0.0.1

---
+++ src/apiCaller.js
@@ -0,0 +1,49 @@
+const createInstanceBtn = document.getElementById('createInstanceBtn');
+
+const {
+    CreateTagsCommand,
+    RunInstancesCommand
+} = require("@aws-sdk/client-ec2");
+import { ec2Client } from "./libs/ec2Client.js";
+
+// Set the parameters
+const instanceParams = {
+    ImageId: "ami-071fe6e60c8fdb961", //AMI_ID
+    InstanceType: "t2.micro",
+    KeyName: "chargeAWS-discord", //KEY_PAIR_NAME
+    MinCount: 1,
+    MaxCount: 1,
+};
+
+const createInstance = async () => {
+    try {
+        const data = await ec2Client.send(new RunInstancesCommand(instanceParams));
+        console.log(data.Instances[0].InstanceId);
+        const instanceId = data.Instances[0].InstanceId;
+        console.log("Created instance", instanceId);
+        // Add tags to the instance
+        const tagParams = {
+            Resources: [instanceId],
+            Tags: [
+                {
+                    Key: "Name",
+                    Value: "Brooooo",
+                },
+                {
+                    Key: "Cert",
+                    Value: "VXE/LXhhaVQ/ZGJwdWdoa0FycTVDbVF1dz9IKWQ0M1g=",
+                },
+            ],
+        };
+        try {
+            const data = await ec2Client.send(new CreateTagsCommand(tagParams));
+            console.log("Instance tagged");
+        } catch (err) {
+            console.log("Error", err);
+        }
+    } catch (err) {
+        console.log("Error", err);
+    }
+};
+
+createInstanceBtn.onclick = createInstance;
--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
   app.quit();
 }
 
-const createWindow = () => {
+const createLicenseKeyWindow = () => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+  const licenseKeyWindow = new BrowserWindow({
+    width: 600,
+    height: 400,
+    frame: false,
+    resizable: false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -19,16 +21,36 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  licenseKeyWindow.loadFile(path.join(__dirname, 'loginScreen.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //licenseKeyWindow.webContents.openDevTools();
+};
+
+const createLoginWindow = () => {
+  // Create the browser window.
+  const loginWindow = new BrowserWindow({
+    width: 600,
+    height: 400,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
+    }
+  });
+
+  // and load the index.html of the app.
+  loginWindow.loadFile(path.join(__dirname, 'loginScreen.html'));
+
+  // Open the DevTools.
+  //loginWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', createLicenseKeyWindow);
+app.on('ready', createLoginWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -43,7 +65,7 @@ app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createLoginWindow();
   }
 });
 
