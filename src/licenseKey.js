ae1691804214abe2208bfbed6e00ebc2ab67aad5
Nolan <nolanmbridges@gmail.com>
2021-06-05 00:14:44 -0700

    win

--- src/licenseKey.js
+++ src/licenseKey.js
@@ -1,17 +1,21 @@
 const keyTextBox = document.getElementById('keyTextBox');
 const returnBtn = document.getElementById('returnBtn');
+returnBtn.onclick = keySearched;
 
+import { machineIdSync } from "../node_modules/node-machine-id/index.js";
 var mysql = require('mysql');
 
+// Checks for 'return' on license key text box
 keyTextBox.addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode == 13) {
         returnBtn.click();
     }
 });
-returnBtn.onclick = keySearched;
 
+// Checks that the input license key is valid
 function keySearched() {
+    // Connects to MySQL server
     var con = mysql.createConnection({
         host: "3.221.75.247",
         user: "typaCC",
@@ -22,6 +26,10 @@ function keySearched() {
         if (err) throw err;
         console.log("Connected!");
     });
+
+    // Gets unique machine ID
+    var machId = machineIdSync({original: true});
+    console.log("MACHINE ID:", machId);
     
     // check if license key is valid
 }
